db32d712be9daef3846e8839ba4fc6796a4589b1
tommytommy-ヒーロー <109708488+tommytommy-hero@users.noreply.github.com>
2022-07-26 02:21:48 +0000

    chapter8-buzz_fizz

---
+++ greeting.rb
@@ -0,0 +1,7 @@
+def greeting(name)
+  return "Hello,#{name}!" #nameは引数で渡すname
+  "Good morning, #{name}"
+
+end
+
+puts greeting('John')
---
+++ lesson7.rb
@@ -0,0 +1,2 @@
+puts "webcamp".upcase #小文字を大文字に
+puta "WEBCAMP".downcase #大文字を小文字に
